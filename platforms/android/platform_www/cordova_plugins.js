29066a45e4afc1613a2e3dfcfe8f67ae76ba4f39
Rafael Junio Xavier <rafael-954@hotmail.com>
2019-11-23 00:55:14 -0400

    Database verify

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-sqlite-storage.SQLitePlugin",
+      "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+      "pluginId": "cordova-sqlite-storage",
+      "clobbers": [
+        "SQLitePlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-sqlite-storage": "3.4.1-dev",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
